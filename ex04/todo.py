5b2c7b2f0da9d7c17a1b47799bfb23b4ba264159
Lucas Peres <lucasperes95@gmail.com>
2023-03-27 00:48:12 -0300

    added solution to 'citations' and 'fullCitations'

--- ex04/todo.py
+++ ex04/todo.py
@@ -1,3 +1,5 @@
+from functools import reduce
+
 def lastNames(L):
     """Mapeia uma lista de nomes para o ultimo nome de cada item.
     Por exemplo, seja:
@@ -18,8 +20,15 @@ def citations(L):
     L = [['Antonio', 'Franco'], ['Caius', 'Vitelus'], ['Cristovao', 'Buarque']]
     entao citations(L) = ['A. Franco', 'C. Vitelus', 'C. Buarque']
     Note que a primeira letra precisa estar capitalizada.
+    >>> list(citations([]))
+    []
+    >>> list(citations([['Antonio', 'Franco']]))
+    ['A. Franco']
+    >>> list(citations([['Antonio', 'Franco'], ['Caius', 'Vitelus'], ['Cristovao', 'Buarque']]))
+    ['A. Franco', 'C. Vitelus', 'C. Buarque']
     """
-    return None
+    initial = lambda e: e[0].capitalize() + '.'
+    return map(lambda e: initial(e[0]) + ' ' + e[-1], L)
 
 def fullCitations(L):
     """Mapeia uma lista de nomes para as iniciais mais o ultimo nome.
@@ -30,5 +39,14 @@ def fullCitations(L):
         ['cristovao', 'buarque', 'Holanda']]
     entao fullCitations(L) = ['A. F. Molina', 'C. V. Aquarius', 'C. B. Holanda']
     Note que as iniciais precisam estar capitalizada.
+    >>> list(fullCitations([]))
+    []
+    >>> list(fullCitations([['Antonio', 'Franco', 'Molina']]))
+    ['A. F. Molina']
+    >>> list(fullCitations([['Antonio', 'Franco', 'Molina'], ['Caius', 'Vitelus', 'Aquarius'], ['Cristovao', 'Buarque']]))
+    ['A. F. Molina', 'C. V. Aquarius', 'C. Buarque']
     """
-    return None
+    initial = lambda e: e[0].capitalize() + '.'
+    initials = lambda e: ' '.join(map(initial, e.split()))
+    citation = lambda s: reduce(lambda acc, e: initials(acc) + ' ' + e, s)
+    return map(citation, L)
